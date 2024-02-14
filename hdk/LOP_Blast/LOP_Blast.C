32c707fd4aa4e6c3f9ca4450bf2e923c8ea9ae37
Michael Reda <eng.michaelreda@gmail.com>
2024-02-14 19:52:33 +1100

    Add UI

--- hdk/LOP_Blast/LOP_Blast.C
+++ hdk/LOP_Blast/LOP_Blast.C
@@ -4,7 +4,7 @@
 
 #include <cmath>
 
-#include "LOP_Ripples.h"
+#include "LOP_Blast.h"
 #include <LOP/LOP_PRMShared.h>
 #include <LOP/LOP_Error.h>
 #include <HUSD/HUSD_CreatePrims.h>
@@ -25,198 +25,90 @@
 
 
 using namespace HOU_HDK;
-// using namespace std;
+using namespace std;
 PXR_NAMESPACE_USING_DIRECTIVE
 
 void newLopOperator(OP_OperatorTable *table){
 
     OP_Operator *op = new OP_Operator(
-        "lop_Ripples",               // Internal name
-        "Ripples",                   // UI name
-        LOP_Ripples::myConstructor,  // How to build the node
-        LOP_Ripples::myTemplateList, // parameters
+        "lop_Blast",               // Internal name
+        "Blast",                   // UI name
+        LOP_Blast::myConstructor,  // How to build the node
+        LOP_Blast::myTemplateList, // parameters
         0,                        // Min # of sources
         1,                        // Max # of sources
         nullptr,                  // Custom local variables (none)
         OP_FLAG_GENERATOR         // Flag it as generator
         );
-        op->setIconName("LOP_Ripples.png");
+        op->setIconName("LOP_Blast.png");
     table->addOperator(op);            
 }
 
 
 
 // Parms
-static PRM_Name primpathName("primpath", "Prim Path");
+static PRM_Name     primpathName("primpath", "Prim Path");
+static PRM_Name     attrnameParmName("attrname", "Attribute");
+static PRM_Name     signParmName("sign", "");
+static PRM_Name     attrvalParmName("attrval", "");
+static PRM_Name     invertParmName("invert", "Invert");
 
-static PRM_Name     propagationParmName("propagation", "Propagation");
-static PRM_Default  propagationParamDefault = PRM_Default(0.05);
-static PRM_Range    propagationParamRange(PRM_RANGE_UI , 0, PRM_RANGE_UI , 1);
+static PRM_Name     signParmTypes[] =
+{
+    PRM_Name("==", "=="),
+    PRM_Name("<", "<"),
+    PRM_Name(">", ">"),
+    PRM_Name(0)
+};
+static PRM_ChoiceList   signTypesMenu(PRM_CHOICELIST_SINGLE, signParmTypes);
 
-PRM_Template LOP_Ripples::myTemplateList[] = {
-    PRM_Template(PRM_STRING,       1, &lopPrimPathName,     &lopEditPrimPathDefault, &lopPrimPathMenu,0, 0, &lopPrimPathSpareData),
-    PRM_Template(PRM_FLT,          1, &propagationParmName,     &propagationParamDefault,       0,  &propagationParamRange),
+
+static PRM_Default  attrnameParmDefault(0.0f, "");
+static PRM_Default  attrvalParmDefault(0.0);
+static PRM_Default  invertParmDefault = PRM_Default(false);
+
+PRM_Template LOP_Blast::myTemplateList[] = {
+    PRM_Template(PRM_STRING,                            1, &lopPrimPathName,         &lopEditPrimPathDefault, &lopPrimPathMenu,0, 0, &lopPrimPathSpareData),
+    PRM_Template(PRM_STRING | PRM_TYPE_JOIN_NEXT,       1, &attrnameParmName,        &attrnameParmDefault),
+    PRM_Template(PRM_ORD | PRM_TYPE_JOIN_NEXT,          1, &signParmName,           0,  &signTypesMenu),
+    PRM_Template(PRM_FLT,                               1, &attrvalParmName,         &attrvalParmDefault),
+    PRM_Template(PRM_TOGGLE,                            1, &invertParmName,          &invertParmDefault),
+
     PRM_Template(),
 };
 
-OP_Node* LOP_Ripples::myConstructor(OP_Network *net, const char *name, OP_Operator *op){
-    return new LOP_Ripples(net, name, op);
+OP_Node* LOP_Blast::myConstructor(OP_Network *net, const char *name, OP_Operator *op){
+    return new LOP_Blast(net, name, op);
 }
 
-LOP_Ripples::LOP_Ripples(OP_Network *net, const char *name, OP_Operator *op): LOP_Node(net, name, op){
+LOP_Blast::LOP_Blast(OP_Network *net, const char *name, OP_Operator *op): LOP_Node(net, name, op){
 }
 
-LOP_Ripples::~LOP_Ripples(){}
+LOP_Blast::~LOP_Blast(){}
 
-void LOP_Ripples::PRIMPATH(UT_String &str, fpreal t){
+void LOP_Blast::PRIMPATH(UT_String &str, fpreal t){
     evalString(str, lopPrimPathName.getToken(), 0, t);
     HUSDmakeValidUsdPath(str, true);
 }
 
-UT_Vector3 LOP_Ripples::getXYZParameterValue(const PRM_Name& parmName, fpreal t)
-{
-    UT_Vector3 value(0.0, 0.0, 0.0);
 
-    for (int i = 0; i < 3; ++i)
-    {
-        fpreal componentValue = evalFloat(parmName.getToken(), i, t);
-        value[i] = componentValue;
-    }
-
-    return value;
+string LOP_Blast::evalAsString(const PRM_Name& parmName, fpreal t){
+    UT_String name;
+    evalString(name, parmName.getToken(), 0, t);
+    return name.toStdString();
 }
 
-
-OP_ERROR LOP_Ripples::cookMyLop(OP_Context &context){
+OP_ERROR LOP_Blast::cookMyLop(OP_Context &context){
     if (cookModifyInput(context) >= UT_ERROR_FATAL){return error();}
 
     fpreal now = context.getTime();
-
-    UT_String yattrname = "primvars:y"; 
-    UT_String laplaceattrname = "primvars:laplace"; 
-
     UT_String primpath;
     PRIMPATH(primpath, now);
 
-    fpreal propagation = evalFloat(propagationParmName.getToken(), 0, now);
-
-    // USD
-    HUSD_AutoWriteLock writelock(editableDataHandle());
-    HUSD_AutoLayerLock layerlock(writelock);
-
-    // Create a helper class for creating USD primitives on the stage.
-    HUSD_CreatePrims creator(layerlock);
-
-    UsdStageRefPtr stage = writelock.data()->stage();
-    UsdPrim prim = stage->GetPrimAtPath(SdfPath(HUSDgetSdfPath(primpath)));
-
-    if (! prim.IsValid()){
-        addError(LOP_PRIM_NOT_FOUND, primpath);
-        return error();
-    }
-
-    VtArray<GfVec3f> points;
-    VtArray<int> points_fv_count;
-    VtArray<int> points_fv_indices;
-    VtArray<float> yvalues;
-    VtArray<float> laplacevalues;
-
-    UsdGeomMesh mesh(prim);
-    UsdAttribute pointsAttr = mesh.GetPointsAttr();
-    UsdAttribute pointsFVAttr = mesh.GetFaceVertexCountsAttr();
-    UsdAttribute pointsFVIAttr = mesh.GetFaceVertexIndicesAttr();
-    UsdAttribute yAttr = prim.GetAttribute(TfToken(yattrname));
-    UsdAttribute laplaceAttr = prim.GetAttribute(TfToken(laplaceattrname));
-
-
-    pointsAttr.Get(&points, now);
-    pointsFVAttr.Get(&points_fv_count, now);
-    pointsFVIAttr.Get(&points_fv_indices, now);
-
-
-    VtArray<float> zerovalues(points.size(), 0.0f);
-
-    if ( ! yAttr.HasValue()){
-        yAttr = prim.CreateAttribute(TfToken(yattrname), SdfValueTypeNames->FloatArray, true);
-        yAttr.SetMetadata(TfToken("interpolation"), VtValue("vertex"));
-        yAttr.Set(zerovalues);
-    }
-
-    if ( ! laplaceAttr.HasValue()){
-        laplaceAttr = prim.CreateAttribute(TfToken(laplaceattrname), SdfValueTypeNames->FloatArray, true);
-        laplaceAttr.SetMetadata(TfToken("interpolation"), VtValue("vertex"));
-        laplaceAttr.Set(zerovalues);
-    }
-    
-
-    // Get points indcies according to faces
-    VtArray<VtIntArray> faces;
-    int fv=0;
-    for(int f=0; f<points_fv_count.size(); f++){
-        int end_idx = fv + points_fv_count[f];
-
-        VtIntArray face(points_fv_indices.begin() + fv, points_fv_indices.begin() + end_idx);
-        faces.push_back(face);
-        fv = end_idx;
-    }
-
-    // Get the faces that contain the same point
-    VtArray<VtArray<VtIntArray>> connected_faces;
-    for (int i=0; i<points.size(); i++){
-        VtArray<VtIntArray> connected_face;
-        for (const auto& face : faces) {
-            if (std::any_of(face.begin(), face.end(), [i](int value) { return value == i; })) {
-                connected_face.push_back(face);
-            }
-        }
-
-        connected_faces.push_back(connected_face);
-
-    }
-
-
-    for (int t=0; t<100; t++){
-
-        pointsAttr.Get(&points, t-1);
-        laplaceAttr.Get(&laplacevalues, t-1);
-        yAttr.Get(&yvalues, t-1);
-
-        VtFloatArray laplace_values;
-        for(int i=0; i<points.size(); i++){
-            float laplace = 0.0;
-
-            for(int f=0; f< connected_faces[i].size(); f++){
-                for(int p=0; p<connected_faces[i][f].size(); p++){
-                    if (connected_faces[i][f][p] == i){
-                        continue;
-                    }
-                    int pt_idx = connected_faces[i][f][p];
-                    laplace += points[pt_idx][1] - points[i][1];
-                }
-            }
-
-            laplace_values.push_back(laplace);
-
-        }
-
-
-        // propagation
-        for (int i=0; i<laplace_values.size(); i++){
-
-            laplace_values[i] *= propagation;
-            yvalues[i] += laplace_values[i];
-            points[i][1] += yvalues[i];
-
-        }
-        
-        laplaceAttr.Set(laplace_values, t);
-        yAttr.Set(yvalues, t);
-        pointsAttr.Set(points, t);
-
-    }
-
-
-
+    string attrname = evalAsString(attrnameParmName, now);
+    string sign = evalAsString(signParmName, now);
+    fpreal attrval = evalFloat(attrvalParmName.getToken(), 0, now);
+    int invert = bool(evalInt(invertParmName.getToken(), 0, now));
 
     return error();
 
--- hdk/LOP_Blast/LOP_Blast.h
+++ hdk/LOP_Blast/LOP_Blast.h
@@ -1,15 +1,15 @@
-#ifndef __LOP_Ripples_h__
-#define __LOP_Ripples_h__
+#ifndef __LOP_Blast_h__
+#define __LOP_Blast_h__
 
 #include <LOP/LOP_Node.h>
 
 namespace HOU_HDK
 {
-    class LOP_Ripples: public LOP_Node{
+    class LOP_Blast: public LOP_Node{
 
         public:
-            LOP_Ripples(OP_Network *net, const char *name, OP_Operator *op);
-            ~LOP_Ripples() override;
+            LOP_Blast(OP_Network *net, const char *name, OP_Operator *op);
+            ~LOP_Blast() override;
 
             static PRM_Template	 myTemplateList[];
             static OP_Node	*myConstructor(OP_Network *net, const char *name, OP_Operator *op);
@@ -19,7 +19,7 @@ namespace HOU_HDK
             OP_ERROR cookMyLop(OP_Context &context) override;
 
             UT_Vector3 getXYZParameterValue(const PRM_Name& parmName, fpreal t);
-            std::string evalOrdAsString(const PRM_Name& parmName, fpreal t);
+            std::string evalAsString(const PRM_Name& parmName, fpreal t);
 
         private:
             void PRIMPATH(UT_String&str, fpreal t);
